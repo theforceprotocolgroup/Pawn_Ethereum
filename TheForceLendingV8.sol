b4cc81966bb544f7b26f537a93d2a10a2417e5d8
Master-ly <timroyal1999@gmail.com>
2019-10-09 17:59:59 +0800

    Update TheForceLendingV8.sol

--- TheForceLendingV8.sol
+++ TheForceLendingV8.sol
@@ -45,3 +45,10 @@ contract EIP20Interface {
     /// @param _value The amount of token to be transferred
     /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
